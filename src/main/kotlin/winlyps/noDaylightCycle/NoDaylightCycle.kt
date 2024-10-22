9b5923b5a8cb19fc70c014cd75baebddcc0f071f
Winlypse <nts3@o2.pl>
2024-10-22 14:23:20 +0200

    Changes!

--- src/main/kotlin/winlyps/noDaylightCycle/NoDaylightCycle.kt
+++ src/main/kotlin/winlyps/noDaylightCycle/NoDaylightCycle.kt
@@ -6,9 +6,14 @@ class NoDaylightCycle : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doDaylightCycle", "false")
+        }
+        logger.info("NoDaylightCycle plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoDaylightCycle plugin has been disabled.")
     }
 }
