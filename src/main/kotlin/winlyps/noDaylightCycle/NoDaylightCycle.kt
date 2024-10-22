a2f5d01a3904b5d5979c09b0a1828b849b3a9674
Winlypse <nts3@o2.pl>
2024-10-22 14:23:06 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noDaylightCycle/NoDaylightCycle.kt
@@ -0,0 +1,14 @@
+package winlyps.noDaylightCycle
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoDaylightCycle : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
